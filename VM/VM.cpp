fe8ee59e1224429405a98817f835f64193aa9661
artur458 <broartur88@gmail.com>
2026-08-27 14:25:58 +0500

    add window

--- VM/VM.cpp
+++ VM/VM.cpp
@@ -1,20 +1,33 @@
-﻿// VM.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-#include <iostream>
+SDL_Window* window;
+SDL_Renderer* renderer;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer("VM", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer);
+	return SDL_APP_CONTINUE;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+SDL_AppResult SDL_AppIterate(void* appstate) {
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderClear(renderer);
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	SDL_RenderPresent(renderer);
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+	if (event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;
+	}
+	return SDL_APP_CONTINUE;
+}
+
+
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+
+}
