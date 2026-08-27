31d3f99eb0a219cec0f5ff0d224836f7a68f77d5
artur458 <broartur88@gmail.com>
2026-08-27 14:46:39 +0500

    add ttf and ui

--- VM/VM.cpp
+++ VM/VM.cpp
@@ -1,12 +1,19 @@
 ﻿#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_image/SDL_image.h>
+
 
 SDL_Window* window;
 SDL_Renderer* renderer;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
+	TTF_Init();
+
+
+
 	SDL_CreateWindowAndRenderer("VM", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer);
 	return SDL_APP_CONTINUE;
 }
