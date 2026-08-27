1cf085161a5e561c1dcb12f42030173c98261ed0
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-27 13:50:53 +0200

    headers

--- cpu.h
+++ cpu.h
@@ -1,1 +1,6 @@
-#include <stdio.h>
+#ifndef CPU_H // Защита - если CPU_H не обновлялся, идем дальше
+#define CPU_H // печать что файл уже подключен
+
+#include <stdio.h> // вывод текста в консоль
+#include <stdint.h> // точные типы данных
+#include <stdbool.h> // работа с тру \ фолс
