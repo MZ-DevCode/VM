ad3799158d935054070f0b9ed4cf7cb52e2ff1ba
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-28 09:46:33 +0200

    cpu init

--- src/cpu.c
+++ src/cpu.c
@@ -2,7 +2,13 @@
 #include <string.h>
 
 void cpu_init(Emulator8086 *emu){
+	memset(emu->memory, 0, sizeof(emu->memory));
+	memset(&emu->cpu, 0, sizeof(Registers8086));	
 
+	emu->cpu.cs = 0xF000;
+	emu->cpu.ip = 0x0000;
+
+	emu->running = true;
 };
 
 uint8_t cpu_read_byte(Emulator8086 *emu, uint32_t physical_address){
