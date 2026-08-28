45933914c20d0e04f6844634fc71926961cf59f0
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-28 09:16:50 +0200

    cpu

---
+++ src/cpu.c
@@ -0,0 +1,14 @@
+#include "cpu.h"
+#include <string.h>
+
+void cpu_init(Emulator8086 *emu){
+
+};
+
+uint8_t cpu_read_byte(Emulator8086 *emu, uint32_t physical_address){
+
+};
+
+void cpu_write_byte(Emulator8086 *emu, uint32_t physical_address, uint8_t val){
+
+};
