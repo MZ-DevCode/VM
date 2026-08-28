efdedc003509253616c16df520efcaf0c16aec76
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-28 18:45:24 +0200

    fix

--- src/cpu.c
+++ src/cpu.c
@@ -5,7 +5,7 @@ void cpu_init(Emulator8086 *emu){
 	memset(emu->memory, 0, sizeof(emu->memory));
 	memset(&emu->cpu, 0, sizeof(Registers8086));	
 
-	emu->cpu.cs = 0xF000;
+	emu->cpu.cs = 0xFFFF;
 	emu->cpu.ip = 0x0000;
 
 	emu->running = true;
