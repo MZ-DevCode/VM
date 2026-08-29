d057b24a433dddda5629a4638086d615242748d6
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-08-29 08:41:34 +0200

    read byte

--- src/cpu.c
+++ src/cpu.c
@@ -12,7 +12,7 @@ void cpu_init(Emulator8086 *emu){
 };
 
 uint8_t cpu_read_byte(Emulator8086 *emu, uint32_t physical_address){
-
+	return emu->memory[physical_address & 0xFFFFF];
 };
 
 void cpu_write_byte(Emulator8086 *emu, uint32_t physical_address, uint8_t val){
